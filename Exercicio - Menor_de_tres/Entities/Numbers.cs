6cbf321509e29703e852d6cd623377920d384ccd
Eduardo Kropniczki <dudukrop@gmail.com>
2024-04-27 11:53:35 -0300

    first commit

---
+++ Exercicio - Menor_de_tres/Entities/Numbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio___Menor_de_tres.Entities
+{
+    class Numbers
+    {
+
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+        public int N3 { get; set; }
+
+        public Numbers(int n1, int n2, int n3)
+        {
+            N1 = n1;
+            N2 = n2;
+            N3 = n3;
+        }
+
+        public int Menor()
+        {
+            if (N1 <= N2 && N1 <= N3)
+                return N1;
+
+            else if (N2 <= N3)
+                return N2;
+
+            else
+                return N3;
+        }
+
+        public override string ToString()
+        {
+            return "Menor = "
+                + Menor();
+        }
+    }
+}
---
+++ Exercicio - Menor_de_tres/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Exercicio___Menor_de_tres.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio___Menor_de_tres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("First Number: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Second Number: ");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.Write("Third Number: ");
+            int n3 = int.Parse(Console.ReadLine());
+
+            Numbers numbers = new Numbers(n1, n2, n3);
+
+            Console.WriteLine(numbers);   
+        }
+    }
+}
